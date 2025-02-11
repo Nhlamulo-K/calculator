58aa4a93afc918163eb68813dedb464becd60a8c
Nhlamulo-K <nhlamokhoza57@gmail.com>
2025-02-11 11:30:48 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
+import math
 
+num1=float(input('Enter first number: '))
+num2=float(input('Enter second number: '))
+operator=input('Enter operator: ')
+
+def math():
+    if operator== '+':
+        num=num1+num2
+    if operator=='-':
+        num=num1-num2
+    if operator=='*':
+        num=num1*num2
+    if operator=='/':
+        if num2!=0:
+            num=num1/num2
+        else:
+            raise ValueError('cannot divide by zero')
+    if operator=='%':
+        num=num1%num2
+    if operator=='sqrt':
+        num=math.sqrt(num1)
+    print(num)
+math()
